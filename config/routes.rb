7b104c9466a49e2b1a45228a57779737bcfb9d85
gagegolden00 <malcolm.golden0@gmail.com>
2023-06-30 14:23:32 -0500

    devise basic setup & pages/landing route/controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#landing"
+  devise_for :users, controllers: {
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
