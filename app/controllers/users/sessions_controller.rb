0eb1635a0f90186d0b4e530a811e77bfae42f49b
gagegolden00 <malcolm.golden0@gmail.com>
2023-07-24 10:00:51 -0500

    fixed a bad login error redirect

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,4 +6,14 @@ class Users::SessionsController < Devise::SessionsController
     yield resource if block_given?
     respond_with resource, location: movies_path
   end
+
+  def new
+      self.resource = resource_class.new(sign_in_params)
+      clean_up_passwords(resource)
+      yield resource if block_given?
+      #respond_with(resource, serialize_options(resource)) # default devise redirect overwritten 
+      redirect_to root_path
+      flash[:notice] = "Invalid Email or Password"
+      end
+
 end
