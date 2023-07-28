28402e0b27285933ce1a41e5d1551811225afcad
gagegolden00 <malcolm.golden0@gmail.com>
2023-07-28 11:39:57 -0500

    updated genre creation service

--- app/services/genre_creator.rb
+++ app/services/genre_creator.rb
@@ -1,10 +1,11 @@
 class GenreCreator
   def self.create_genre(movie_data_hash)
+    existing_genres_in_database = Genre.pluck(:name)
     genre_csv = movie_data_hash["Genre"]
     genre_names = genre_csv.split(", ").map(&:strip)
     genre_names.map do |genre|
-      unless Genre.find_by(name: genre)
-        Genre.new(name: genre)
+      unless existing_genres_in_database.include?(genre)
+        Genre.create(name: genre)
       end
     end
     genre_names
